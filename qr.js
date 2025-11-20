bb26ea26f97acf30a4050e754604bf09f7124a47
TEDDY TECH <kibetke4488@gmail.com>
2025-11-20 13:07:22 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -151,17 +151,17 @@ router.get('/', async (req, res) => {
                             
                             // Send video thumbnail with caption
                             await sock.sendMessage(userJid, {
-                                image: { url: 'https://img.youtube.com/vi/-oz_u1iMgf8/maxresdefault.jpg' },
-                                caption: `🎬 *KnightBot MD V2.0 Full Setup Guide!*\n\n🚀 Bug Fixes + New Commands + Fast AI Chat\n📺 Watch Now: https://youtu.be/-oz_u1iMgf8`
+                                image: { url: 'https://files.catbox.moe/a0mj3n.jpg' },
+                                caption: `🎬 *TEDDY XMD V2.0 Full Setup Guide!*\n\n🚀 Bug Fixes + New Commands + Fast AI Chat\n📺 Watch Now: https://youtu.be/Teddytech4`
                             });
                             console.log("🎬 Video guide sent successfully");
                             
                             // Send warning message
                             await sock.sendMessage(userJid, {
                                 text: `⚠️Do not share this file with anybody⚠️\n 
-┌┤✑  Thanks for using Knight Bot
+┌┤✑  Thanks for using TEDDY-XMD 
 │└────────────┈ ⳹        
-│©2024 Mr Unique Hacker 
+│©2025 Teddy Tech Hub
 └─────────────────┈ ⳹\n\n`
                             });
                         } else {
